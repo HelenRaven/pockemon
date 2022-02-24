d9798b6401daef2d326cb57efb3f2ea078f41dc4
HelenRaven <helenraven@mail.ru>
2022-02-24 12:11:31 +0700

    version 1.0.0

--- pockemon.go
+++ pockemon.go
@@ -2,6 +2,8 @@ package pockemon
 
 import (
 	"fmt"
+
+	_ "github.com/valyala/fasthttp"
 )
 
 type Pokemon struct {
@@ -10,4 +12,8 @@ type Pokemon struct {
 
 func (p *Pokemon) IChooseU() {
 	fmt.Println("I choose u,", p.Name)
+}
+
+func StartFight(p1, p2 *Pokemon) {
+	fmt.Printf("Start fight! %s VS %s", p1.Name, p2.Name)
 }
